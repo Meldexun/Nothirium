b33aa06e2317e4c52154680e60eb890533527e2b
Meldexun <27779321+Meldexun@users.noreply.github.com>
2022-05-26 19:25:04 +0200

    Update renderlib dependency to 1.0.6

--- src/main/java/meldexun/nothirium/mc/Nothirium.java
+++ src/main/java/meldexun/nothirium/mc/Nothirium.java
@@ -11,7 +11,7 @@ import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLConstructionEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 
-@Mod(modid = Nothirium.MODID, dependencies = "required:renderlib@[1.0.5,)")
+@Mod(modid = Nothirium.MODID, dependencies = "required:renderlib@[1.0.6,)")
 public class Nothirium {
 
 	public static final String MODID = "nothirium";
