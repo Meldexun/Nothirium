caa964ffb0ce25f6a1041f4f949dbfed4e881f81
Meldexun <27779321+Meldexun@users.noreply.github.com>
2022-06-22 13:39:21 +0200

    Resort transparency when camera location changes

--- src/main/java/meldexun/nothirium/renderer/chunk/AbstractChunkRenderer.java
+++ src/main/java/meldexun/nothirium/renderer/chunk/AbstractChunkRenderer.java
@@ -10,14 +10,13 @@ import org.lwjgl.opengl.GL11;
 
 import meldexun.nothirium.api.renderer.chunk.ChunkRenderPass;
 import meldexun.nothirium.api.renderer.chunk.IChunkRenderer;
+import meldexun.nothirium.api.renderer.chunk.IRenderChunkDispatcher;
 import meldexun.nothirium.api.renderer.chunk.IRenderChunkProvider;
 import meldexun.nothirium.mc.renderer.ChunkRenderManager;
 import meldexun.nothirium.util.Direction;
 import meldexun.nothirium.util.collection.Enum2ObjMap;
 import meldexun.nothirium.util.math.MathUtil;
 import meldexun.renderlib.util.Frustum;
-import net.minecraft.client.Minecraft;
-import net.minecraft.entity.Entity;
 
 public abstract class AbstractChunkRenderer<T extends AbstractRenderChunk<T>> implements IChunkRenderer<T> {
 
@@ -38,30 +37,11 @@ public abstract class AbstractChunkRenderer<T extends AbstractRenderChunk<T>> im
 
 	@Override
 	public void setup(IRenderChunkProvider<T> renderChunkProvider, double cameraX, double cameraY, double cameraZ, Frustum frustum, int frame) {
+		this.resortTransparency(cameraX, cameraY, cameraZ, frustum);
+
 		int chunkX = MathUtil.floor(cameraX) >> 4;
 		int chunkY = MathUtil.floor(cameraY) >> 4;
 		int chunkZ = MathUtil.floor(cameraZ) >> 4;
-
-		Entity entity = Minecraft.getMinecraft().getRenderViewEntity();
-		double dx = entity.posX - lastTransparencyResortX;
-		double dy = entity.posY - lastTransparencyResortY;
-		double dz = entity.posZ - lastTransparencyResortZ;
-		if (dx * dx + dy * dy + dz * dz > 1.0D) {
-			lastTransparencyResortX = entity.posX;
-			lastTransparencyResortY = entity.posY;
-			lastTransparencyResortZ = entity.posZ;
-			int r = 2;
-			for (int x = -r; x <= r; x++) {
-				for (int y = -r; y <= r; y++) {
-					for (int z = -r; z <= r; z++) {
-						T renderChunk = renderChunkProvider.getRenderChunkAt(chunkX + x, chunkY + y, chunkZ + z);
-						if (renderChunk.isFrustumCulled(frustum))
-							continue;
-						renderChunk.resortTransparency(this, ChunkRenderManager.getTaskDispatcher());
-					}
-				}
-			}
-		}
 
 		chunks.forEach(List::clear);
 		T rootRenderChunk = renderChunkProvider.getRenderChunkAt(chunkX, chunkY, chunkZ);
@@ -96,6 +76,34 @@ public abstract class AbstractChunkRenderer<T extends AbstractRenderChunk<T>> im
 					chunkQueue.add(neighbor);
 				}
 				neighbor.setOrigin(direction.opposite());
+			}
+		}
+	}
+
+	private void resortTransparency(double cameraX, double cameraY, double cameraZ, Frustum frustum) {
+		double dx = cameraX - lastTransparencyResortX;
+		double dy = cameraY - lastTransparencyResortY;
+		double dz = cameraZ - lastTransparencyResortZ;
+		if (dx * dx + dy * dy + dz * dz > 1.0D) {
+			lastTransparencyResortX = cameraX;
+			lastTransparencyResortY = cameraY;
+			lastTransparencyResortZ = cameraZ;
+
+			IRenderChunkProvider<T> provider = ChunkRenderManager.getProvider();
+			IRenderChunkDispatcher taskDispatcher = ChunkRenderManager.getTaskDispatcher();
+			int chunkX = MathUtil.floor(cameraX) >> 4;
+			int chunkY = MathUtil.floor(cameraY) >> 4;
+			int chunkZ = MathUtil.floor(cameraZ) >> 4;
+			int r = 2;
+			for (int x = -r; x <= r; x++) {
+				for (int y = -r; y <= r; y++) {
+					for (int z = -r; z <= r; z++) {
+						T renderChunk = provider.getRenderChunkAt(chunkX + x, chunkY + y, chunkZ + z);
+						if (renderChunk.isFrustumCulled(frustum))
+							continue;
+						renderChunk.resortTransparency(this, taskDispatcher);
+					}
+				}
 			}
 		}
 	}
